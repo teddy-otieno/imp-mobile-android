2c6f7b35ec131296f5d06cd40604489b4c20d0bb
Teddy Juma <tedd.juma@outlook.com>
2020-05-16 13:36:10 +0300

    Fixed the bottomsheet bar
    Signed-off-by: Teddy Juma <tedd.juma@outlook.com>

--- app/src/main/java/com/imp/impandroidclient/campaign/CampaignActivity.kt
+++ app/src/main/java/com/imp/impandroidclient/campaign/CampaignActivity.kt
@@ -1,23 +1,20 @@
 package com.imp.impandroidclient.campaign
 
-import android.animation.AnimatorSet
-import android.animation.ObjectAnimator
-import android.animation.TimeInterpolator
-import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.imp.impandroidclient.R
 import com.imp.impandroidclient.app_state.Cache
-import com.imp.impandroidclient.image_submission.ImageSubmission
 import kotlinx.android.synthetic.main.activity_campaign.*
-import kotlin.math.pow
+import kotlinx.android.synthetic.main.layout_choose_submission.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class CampaignActivity : AppCompatActivity() {
 
@@ -28,14 +25,38 @@ class CampaignActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_campaign)
 
+        initMembers()
+        setUpObservers()
+        setUpListeners()
+        setupBottomSheet()
+
+        lifecycleScope.launchWhenCreated {
+            val campaign = viewModel.getCampaign().value!!
+        }
+
+    }
+
+    private fun initMembers() {
         val campaignId: Int = intent.getIntExtra("campaignId", -1)
 
         if(campaignId == -1) {
             throw IndexOutOfBoundsException("CampaignID is out of range")
         }
         campaignModelFactory = CampaignViewModelFactory(campaignId)
-
         viewModel = ViewModelProvider(this, campaignModelFactory).get(CampaignViewModel::class.java)
+    }
+
+    private fun setUpListeners() {
+
+        /*
+        campaignSubmission.setOnClickListener {
+            setupBottomSheet()
+        }
+         */
+
+    }
+
+    private fun setUpObservers() {
 
         viewModel.getCampaign().observe(this, Observer { campaign ->
             detailed_campaignCoverImage.setImageBitmap(Cache.getImageFromMemCache(campaign.cover_image))
@@ -43,6 +64,25 @@ class CampaignActivity : AppCompatActivity() {
             detailed_campaignDescription.text = campaign.about_you
             detailed_contentWedLoveFromYou.text = campaign.content_wed_love_from_you
             detailed_whereToFindProduct.text = campaign.where_to_find_product
+
+
+            val cachedBrandImage = Cache.getImageFromMemCache(campaign.brand.brand_image)
+            if(cachedBrandImage == null) {
+                lifecycleScope.launch(Dispatchers.Main) {
+                    //TODO: Handle this instead of crashing the app
+                    val brandImage = campaign.brand.brandImageFuture?.await()
+
+                    if(brandImage == null) {
+                        TODO("Handle this error")
+                    } else {
+                        brandOfCampaign.setImageBitmap(brandImage)
+                        brandOfCampaign.clipToOutline = true
+                    }
+                }
+            } else {
+                brandOfCampaign.setImageBitmap(cachedBrandImage)
+            }
+
 
             for (item in campaign.dos) {
                 val view: ConstraintLayout =
@@ -63,49 +103,27 @@ class CampaignActivity : AppCompatActivity() {
             }
 
         })
-
-        submissionTypeChooser.visibility = View.GONE
-        setUpListeners()
-
-        lifecycleScope.launchWhenCreated {
-
-            val campaign = viewModel.getCampaign().value!!
-
-
-        }
     }
 
-    private fun setUpListeners() {
-        val imageSubmission: Button = submissionTypeChooser.findViewById(R.id.imageSubmissionButton)
-        imageSubmission.setOnClickListener {
-            //TODO("Switch to image submission activity")
+    private fun setupBottomSheet() {
 
-            val intent = Intent(this, ImageSubmission::class.java)
-            intent.putExtra("campaignId", viewModel.campaignId)
+        val sheetBehavior = BottomSheetBehavior.from(submissionTypeChooser)
 
-            this.startActivity(intent)
-        }
+        sheetBehavior.addBottomSheetCallback(object: BottomSheetBehavior.BottomSheetCallback() {
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
 
-        val videoSubmission: Button = submissionTypeChooser.findViewById(R.id.videoSubmissionButton)
-        videoSubmission.setOnClickListener {
-            TODO("Switch to video capture submission")
-        }
-
-        campaignSubmission.setOnClickListener {
-            submissionTypeChooser.visibility = View.VISIBLE
-
-            val animatorSet: AnimatorSet = AnimatorSet()
-
-            val animation: ObjectAnimator =
-                ObjectAnimator.ofFloat(submissionTypeChooser, "translationY", 200.0f, 0.0f)
-            animation.duration = 500
-            animation.interpolator = TimeInterpolator { x ->
-                (1 - (1.0 - x).pow(2.0)).toFloat()
             }
 
-            animatorSet.play(animation)
-            animatorSet.start()
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+            }
+        })
 
+        submissionTypeChooser.setOnClickListener {
+            if(sheetBehavior.state  != BottomSheetBehavior.STATE_EXPANDED) {
+                sheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+            } else {
+                sheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+            }
         }
     }
 }
